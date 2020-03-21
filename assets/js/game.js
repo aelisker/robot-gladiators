d702b4a6313ab87bb1bc47769849cf426c8957b2
Aaron Lisker <aelisker@gmail.com>
2020-03-21 13:41:39 -0700

    first function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+//Console.log examples
+console.log("This is a string");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
